ef158fe2b306a53271b303a678f177e77ef6532d
Pixeleur <thomcodeunlimited@gmail.com>
2023-03-21 15:50:13 +0100

    last

--- main.js
+++ main.js
@@ -36,8 +36,55 @@ anime.timeline({loop: false})
       $('.main').hide(0).delay(10000).show(0);
       });
       
-      jQuery(document).ready(function(){
+   jQuery(document).ready(function(){
 
         $('#essai').hide(0).delay(5000).show(5000);
       });
+
+let searchButton1 = document.querySelector('#search1')
+let searchButton2 = document.querySelector('#search2')
+let API_KEY = 'YzL4w4T17cLoLjPhg06eF7WGQVKlhbMCyrVlYEMr';
+let response = '';
+
+searchButton1.addEventListener('click', ()=>{
+console.log('button pressed')
+sendApiRequest1()
+})
+
+
+
+async function sendApiRequest1(){
+  try {
+    let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`);
+    let data = await response.json();
+    useApiData1(data);
+} catch (error) {
+    console.error(error);
+}
+console.log(response)
+
+console.table(response)
+let data = await response.json()
+console.table(data)
+useApiData1(data)
+} 
+
+
+
+
+function useApiData1(data){
+
+for(i=0; i<=999; i += 98) {
+  
+
+document.querySelector('#content1').innerHTML += `<img id=${imgApi} src=${data.photos[i].img_src}>`;
+}
+
+}
+
+
+
+
+
+
       
